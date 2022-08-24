1a0fb9e25f3fe5bd9dd4770a73bddee89c19fbe9
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-08-24 17:26:29 -0300

    [Feature] Added DbAccess test

--- test/MamisSolidarias.WebAPI.Beneficiaries.Test/DbAccess/Community.Get.cs
+++ test/MamisSolidarias.WebAPI.Beneficiaries.Test/DbAccess/Community.Get.cs
@@ -1,5 +1,8 @@
+using System.Linq;
 using System.Threading.Tasks;
+using FluentAssertions;
 using MamisSolidarias.Infrastructure.Beneficiaries;
+using MamisSolidarias.WebAPI.Beneficiaries.Utils;
 using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
@@ -11,7 +14,8 @@ internal class Community_Get
     private const string InMemoryConnectionString = "DataSource=:memory:";
     
     private BeneficiariesDbContext _dbContext = null!;
-    private Endpoints.Communities.POST.DbAccess _dbAccess = null!;
+    private Endpoints.Communities.GET.DbAccess _dbAccess = null!;
+    private DataFactory _dataFactory = null!;
 
     [SetUp]
     public void TestWithSqlite()
@@ -25,7 +29,8 @@ internal class Community_Get
         _dbContext = new BeneficiariesDbContext(options);
         _dbContext.Database.EnsureCreated();
 
-        _dbAccess = new Endpoints.Communities.POST.DbAccess(_dbContext);
+        _dbAccess = new Endpoints.Communities.GET.DbAccess(_dbContext);
+        _dataFactory = new(_dbContext);
     }
 
     [TearDown]
@@ -38,6 +43,26 @@ internal class Community_Get
     public async Task WithManyValues_Succeeds()
     {
         // Arrange
+        var communities = _dataFactory.GenerateCommunities(5)
+            .Select(t => t.Build())
+            .ToArray();
         
+        // Act
+        var value = await _dbAccess.GetCommunities(default);
+        
+        // Assert
+        value.Should().BeEquivalentTo(communities);
+    }
+    
+    [Test]
+    public async Task WithNoValues_Succeeds()
+    {
+        // Arrange
+
+        // Act
+        var value = await _dbAccess.GetCommunities(default);
+        
+        // Assert
+        value.Should().BeEmpty();
     }
 }
