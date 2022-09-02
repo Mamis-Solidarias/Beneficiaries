4aba51c933f5d3096a64d2fc087f0886b328501b
Lucas Dell'Isola <dev@lucasdellisola.com.ar>
2022-09-02 15:16:25 -0300

    [Feature] Added endpoint

---
+++ src/MamisSolidarias.WebAPI.Beneficiaries/Endpoints/Beneficiaries/Id/POST/DbAccess.cs
@@ -0,0 +1,32 @@
+using MamisSolidarias.Infrastructure.Beneficiaries;
+using MamisSolidarias.Infrastructure.Beneficiaries.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MamisSolidarias.WebAPI.Beneficiaries.Endpoints.Beneficiaries.Id.POST;
+
+internal class DbAccess
+{
+    private readonly BeneficiariesDbContext? _dbContext;
+
+    public DbAccess()
+    {
+    }
+
+    public DbAccess(BeneficiariesDbContext? dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    
+    public virtual Task<Beneficiary?> GetBeneficiary(int id, CancellationToken ct)
+    {
+        ArgumentNullException.ThrowIfNull(_dbContext);
+        return _dbContext.Beneficiaries
+            .FirstOrDefaultAsync(t => t.Id == id, ct);
+    }
+
+    public virtual Task SaveChanges(CancellationToken ct)
+    {
+        ArgumentNullException.ThrowIfNull(_dbContext);
+        return _dbContext.SaveChangesAsync(ct);
+    }
+}
---
+++ src/MamisSolidarias.WebAPI.Beneficiaries/Endpoints/Beneficiaries/Id/POST/Endpoint.cs
@@ -0,0 +1,46 @@
+using FastEndpoints;
+using MamisSolidarias.Infrastructure.Beneficiaries;
+using MamisSolidarias.Infrastructure.Beneficiaries.Models;
+
+
+namespace MamisSolidarias.WebAPI.Beneficiaries.Endpoints.Beneficiaries.Id.POST;
+
+internal class Endpoint : Endpoint<Request>
+{
+    private readonly DbAccess _db;
+
+    public Endpoint(BeneficiariesDbContext dbContext, DbAccess? dbAccess = null)
+    {
+        _db = dbAccess ?? new DbAccess(dbContext);
+    }
+
+    public override void Configure()
+    {
+        Post("beneficiaries/{id}");
+        Policies(Utils.Security.Policies.CanWrite);
+    }
+
+    public override async Task HandleAsync(Request req, CancellationToken ct)
+    {
+        var beneficiary = await _db.GetBeneficiary(req.Id, ct);
+
+        if (beneficiary is null)
+        {
+            await SendNotFoundAsync(ct);
+            return;
+        }
+
+        if (beneficiary.IsActive == true)
+        {
+            AddError("El beneficiario no esta inactivo");
+            await SendErrorsAsync(cancellation:ct);
+        }
+
+        beneficiary.IsActive = true;
+
+        await _db.SaveChanges(ct);
+        await SendOkAsync(ct);
+    }
+
+
+}
